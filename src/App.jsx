8b35ef484d7e3fc33824c825d2bbc529d894032a
Arash <arashsaleh798@gmail.com>
2025-03-14 12:58:55 -0700

    completed all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-
-
-import { Routes, Route } from 'react-router'; // Import React Router
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
-// Import the SignUpForm component
 import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
+// Import the Landing and Dashboard components
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+
+// Import the UserContext
+import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+  // Get the user
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
-      {/* Add the Routes component to wrap our individual routes*/}
       <Routes>
+        <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
         <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path='/sign-in' element={<SignInForm />} />
       </Routes>
     </>
   );
